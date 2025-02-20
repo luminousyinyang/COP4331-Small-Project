e578238ecf6bb273edfeaaf1c6317bc1bc79bf13
Loren P <160626485+lore4prez@users.noreply.github.com>
2025-02-19 20:42:12 -0500

    Add edit contact modal and delete popup

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,6 +1,33 @@
-// Open the modal
-const modalContainer = document.querySelector('#modal');
-const closeModal = document.querySelector('#x-modal');
+// Open the add-contact modal
+const addContactModal = document.querySelector('#add-ct-modal');
+const closeAddModal = document.querySelector('#x-add-modal');
 
-addContactButton.addEventListener('click', () => modalContainer.showModal());
-closeModal.addEventListener('click', () => modalContainer.close());
+addContactButton.addEventListener('click', () => addContactModal.showModal());
+addContactIcon.addEventListener('click', () => addContactModal.showModal());
+closeAddModal.addEventListener('click', () => addContactModal.close());
+
+// Open the edit-contact modal
+const editContactModal = document.querySelector("#edit-ct-modal");
+const closeEditModal = document.querySelector("#x-edit-modal");
+
+// Get all the edit icons
+const editIconsList = document.querySelectorAll(".edit-contact-icon");
+
+editIconsList.forEach((icon) => {
+    icon.addEventListener('click', () => editContactModal.showModal());
+});
+closeEditModal.addEventListener('click', () => editContactModal.close());
+
+
+// Open the delete confirmation popup 
+const deleteContactModal = document.querySelector("#delete-contact-modal");
+const closeDeleteModalList = document.querySelectorAll(".cancel");
+const deleteIconsList = document.querySelectorAll('.delete-contact-icon');
+
+deleteIconsList.forEach((icon) => {
+    icon.addEventListener('click', () => deleteContactModal.showModal());
+});
+
+closeDeleteModalList.forEach((button) => {
+    button.addEventListener('click', () => deleteContactModal.close());
+});
